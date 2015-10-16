03525f89066c5b364e2d5e13552ad283218e44f2
ClementRoosDev <clement.roos@ynov.com>
2015-10-16 17:16:39 +0200

    Update page Git + Start

--- MSLab.Site/Pages/Git.aspx.designer.cs
+++ MSLab.Site/Pages/Git.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace MSLab.Site.Pages {
     public partial class Git {
         
         /// <summary>
-        /// hyperlink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hyperlink1;
-        
-        /// <summary>
         /// hyperlink2 control.
         /// </summary>
         /// <remarks>
